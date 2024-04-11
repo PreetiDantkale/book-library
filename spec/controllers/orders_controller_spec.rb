3afb8c4da4b1ec833b6b9dd1019f88ac081a1736
Preeti Dantkale <Preeti Dantkale>
2024-04-11 16:51:31 +0530

    Update spec

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe OrdersController, type: :controller do
     let(:user) { create(:user) }
     let(:item) { create(:item) }
 
-    context 'when user has not reached maximum transactions' do
+    context 'when all conditions are met' do
+      let(:user_valid) { create(:user) }
+      let(:valid_item) { create(:item) }
+
       before do
-        allow(User).to receive(:find).and_return(user)
-        allow(controller).to receive(:find_item).and_return(item)
+        allow(User).to receive(:find).and_return(user_valid)
+        allow(controller).to receive(:find_item).and_return(valid_item)
+        allow(controller).to receive(:item_available_for_subscription?).and_return(true)
+        allow(user_valid.orders).to receive(:create).and_return(true)
       end
 
-      it 'creates a new order' do
-        post :create, params: { order: { user_id: user.id, item_id: item.id } }
+      it 'creates a new order successfully' do
+        post :create, params: { order: { user_id: user_valid.id, item_id: valid_item.id } }
         expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)['success']).to eq('Order placed successfully')
       end
     end
 
@@ -31,7 +37,122 @@ RSpec.describe OrdersController, type: :controller do
       end
     end
 
-    # Additional contexts for other conditions can be added here
+    context 'when item is already borrowed' do
+      before do
+        allow(User).to receive(:find).and_return(user)
+        allow(controller).to receive(:find_item).and_return(item)
+        allow(item.orders).to receive(:borrowed).and_return(Order.where(id: create(:order, user: user, item: item, status: 'borrowed').id))
+      end
+
+      it 'returns an error for item already borrowed' do
+        post :create, params: { order: { user_id: user.id, item_id: item.id } }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)['error']).to eq('Already borrowed')
+      end
+    end
+
+    context 'when user age is less than 18 and item genre is "crime"' do
+      let(:user_underage) { create(:user, age: 16) }
+      let(:crime_item) { create(:item, genre: 'crime') }
+
+      it 'returns an error for invalid age' do
+        post :create, params: { order: { user_id: user_underage.id, item_id: crime_item.id } }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)['error']).to eq('Age not valid')
+      end
+    end
+
+    context 'when item is not available for user subscription plan' do
+      let(:silver_user) { create(:user, subscription_plan: 'silver') }
+      let(:magazine_item) { create(:item, item_type: 'magazine') }
+
+      before do
+        allow(User).to receive(:find).and_return(silver_user)
+        allow(controller).to receive(:find_item).and_return(magazine_item)
+      end
+
+      it 'returns an error for item not available for subscription plan' do
+        post :create, params: { order: { user_id: silver_user.id, item_id: magazine_item.id } }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)['error']).to eq('The requested item is not available for your subscription plan')
+      end
+    end
+
+    describe 'item_available_for_subscription?' do
+      let(:user) { create(:user) }
+      let(:item) { create(:item) }
+  
+      context 'when user has silver subscription' do
+        it 'returns true for non-magazine item and under transaction limit' do
+          allow(user.orders).to receive_message_chain(:this_month, :where, :count).and_return(1)
+          allow(Item).to receive(:where).and_return([item])
+          expect(controller.send(:item_available_for_subscription?, user, item)).to eq(true)
+        end
+  
+        it 'returns false for magazine item' do
+          magazine_item = create(:item, item_type: 'magazine')
+          expect(controller.send(:item_available_for_subscription?, user, magazine_item)).to eq(false)
+        end
+  
+        it 'returns false for reaching transaction limit' do
+          allow(user.orders).to receive_message_chain(:this_month, :where, :count).and_return(2)
+          allow(Item).to receive(:where).and_return([item])
+          expect(controller.send(:item_available_for_subscription?, user, item)).to eq(false)
+        end
+      end
+  
+      context 'when user has gold subscription' do
+        let(:gold_user) { create(:user, subscription_plan: 'gold') }
+  
+        it 'returns true for non-magazine item, under transaction limit, and magazines borrowed limit' do
+          allow(gold_user.orders).to receive_message_chain(:this_month, :where, :count).and_return(2)
+          allow(Item).to receive(:where).and_return([item])
+          expect(controller.send(:item_available_for_subscription?, gold_user, item)).to eq(true)
+        end
+  
+        it 'returns false for magazine item and reaching transaction limit' do
+          magazine_item = create(:item, item_type: 'magazine')
+          allow(gold_user.orders).to receive_message_chain(:this_month, :where, :count).and_return(2)
+          expect(controller.send(:item_available_for_subscription?, gold_user, magazine_item)).to eq(false)
+        end
+  
+        it 'returns false for non-magazine item and reaching transaction limit' do
+          allow(gold_user.orders).to receive_message_chain(:this_month, :where, :count).and_return(3)
+          allow(Item).to receive(:where).and_return([item])
+          expect(controller.send(:item_available_for_subscription?, gold_user, item)).to eq(false)
+        end
+      end
+  
+      context 'when user has platinum subscription' do
+        let(:platinum_user) { create(:user, subscription_plan: 'platinum') }
+  
+        it 'returns true for non-magazine item, under transaction limit, and magazines borrowed limit' do
+          allow(platinum_user.orders).to receive_message_chain(:this_month, :where, :count).and_return(3)
+          allow(Item).to receive(:where).and_return([item])
+          expect(controller.send(:item_available_for_subscription?, platinum_user, item)).to eq(true)
+        end
+  
+        it 'returns false for magazine item and reaching transaction limit' do
+          magazine_item = create(:item, item_type: 'magazine')
+          allow(platinum_user.orders).to receive_message_chain(:this_month, :where, :count).and_return(3)
+          expect(controller.send(:item_available_for_subscription?, platinum_user, magazine_item)).to eq(false)
+        end
+  
+        it 'returns false for non-magazine item and reaching transaction limit' do
+          allow(platinum_user.orders).to receive_message_chain(:this_month, :where, :count).and_return(4)
+          allow(Item).to receive(:where).and_return([item])
+          expect(controller.send(:item_available_for_subscription?, platinum_user, item)).to eq(false)
+        end
+      end
+  
+      context 'when user has an unknown subscription plan' do
+        let(:unknown_user) { create(:user, subscription_plan: 'unknown') }
+  
+        it 'returns false' do
+          expect(controller.send(:item_available_for_subscription?, unknown_user, item)).to eq(false)
+        end
+      end
+    end
   end
 
   describe 'PATCH #update' do
@@ -45,14 +166,13 @@ RSpec.describe OrdersController, type: :controller do
       before do
         allow(User).to receive(:find).and_return(user)
         allow(Item).to receive(:where).and_return([item1, item2])
-        # allow(user.orders).to receive(:borrowed).and_return(Order.where(id: [borrowed_item1.id, borrowed_item2.id]))
       end
 
       it 'updates the status of borrowed items' do
         patch :update, params: { order: { user_id: user.id }, item_ids: [item1.id, item2.id] }
         expect(response).to have_http_status(:ok)
-        # expect(borrowed_item1.reload.status).to eq('returned')
-        # expect(borrowed_item2.reload.status).to eq('returned')
+        expect(borrowed_item1.reload.status).to eq('returned')
+        expect(borrowed_item2.reload.status).to eq('returned')
       end
     end
 
@@ -69,7 +189,5 @@ RSpec.describe OrdersController, type: :controller do
         expect(JSON.parse(response.body)['error']).to eq('No matching borrowed items found for the user')
       end
     end
-
-    # Additional contexts for other conditions can be added here
   end
 end
